5cf4ad9d6df04a22a9e215e88f328a6819cb11cf
ex.crisanl <laurentiu.crisan@atesion.de>
2018-08-13 15:57:29 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int test = 0;
+
+            
+            while(test++ < 10){
+                Console.WriteLine(string.Format("The value is {0}", test));
+            }
+            System.Diagnostics.Debug.Assert(test == 0);
+        }
+    }
+}
